2886724404a0756152feb4c4a89fae6c1e88a72a
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-07-28 03:37:00 +0100

    Replaced private readonly prop names

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -13,22 +13,22 @@ namespace API.Controllers
     [Route("[controller]")]
     public class ProductsController: ControllerBase
     {
-        private readonly StoreContext context;
+        private readonly StoreContext _context;
         public ProductsController(StoreContext context)
-        {
-            this.context = context;   
+        { 
+            _context = context;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return await context.Products.ToListAsync();
+            return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await context.Products.FindAsync(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
