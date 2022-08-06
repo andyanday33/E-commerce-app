19e3b12fa8ec3b385f27ee3de5026ce8bb1957b4
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-08-06 02:57:29 +0100

    App bar styling updates

--- client/app/layouts/Header.tsx
+++ client/app/layouts/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { AppBar, Divider, FormControl, FormControlLabel, Switch, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Divider, FormControl, FormControlLabel, Grid, Switch, Toolbar, Typography } from '@mui/material';
 
 interface IHeaderProps {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
+const pages = ['Catalog', 'About', 'Contact'];
 
 const Header: React.FC<IHeaderProps> = ({ darkMode, handleThemeChange }) => {
   return (
@@ -26,6 +27,29 @@ const Header: React.FC<IHeaderProps> = ({ darkMode, handleThemeChange }) => {
                     label={darkMode? 'Light Mode': 'Dark Mode'}
                     />
                 </FormControl>
+                <Box 
+                justifyContent='center'
+                alignItems='center'
+                sx={{ 
+                    flexGrow: 1,
+                    display: { 
+                        xs: 'none',
+                        md: 'flex',
+                        position: 'absolute',
+                        left: '0',
+                        right: '0',
+                        mx: 'auto',
+                        } 
+                    }}>
+                    {pages.map((page) => (
+                    <Button
+                        key={page}
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                        {page}
+                    </Button>
+                ))}
+                </Box>
             </Toolbar>
         </AppBar>
     </header>
