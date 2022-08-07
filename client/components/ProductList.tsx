25a2c85bbfe50b3cc34599e70a2e1daeb6dba080
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-08-07 09:20:02 +0100

    Added individual product fetching to catalog/:id route

--- client/components/ProductList.tsx
+++ client/components/ProductList.tsx
@@ -1,13 +1,13 @@
 import { Grid, List } from "@mui/material";
 import React from "react";
-import { IProductListProps } from "../types";
+import { IProductListProps, IProduct } from "../types";
 import ProductCard from "./ProductCard";
 
 const ProductList: React.FC<IProductListProps> = ({ products }) => {
   return (
     <List>
       <Grid container spacing={4}>
-        {products.map((product) => (
+        {products.map((product: IProduct) => (
           <Grid item xs={12} sm={6} lg={3} key={product.id}>
             <ProductCard product={product} />
           </Grid>
--- client/pages/catalog/[id].tsx
+++ client/pages/catalog/[id].tsx
@@ -1,9 +1,28 @@
+import { Typography } from "@mui/material";
+import { useRouter } from "next/router";
 import React from "react";
+import useSWR, { Fetcher } from "swr";
+import { IProduct } from "../../types";
 
 type Props = {};
 
+const fetcher: Fetcher<IProduct, string> = (...args) =>
+  fetch(...args).then((res) => res.json());
+
 const CatalogProduct = (props: Props) => {
-  return <div>CatalogProduct</div>;
+  const router = useRouter();
+  const { id } = router.query;
+  const { data: product, error } = useSWR(
+    `http://localhost:5285/Products/${id}`,
+    fetcher
+  );
+  if (!product && !error) return <Typography>Loading...</Typography>;
+  if (error || !product) return <Typography>Failed to fetch data.</Typography>;
+  return (
+    <div>
+      CatalogProduct #{id} #{product.name}
+    </div>
+  );
 };
 
 export default CatalogProduct;
